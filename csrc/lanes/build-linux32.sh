95351354838b6a2d5417d3187a024d85b8df82ee
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:51:14 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/lanes/build-linux32.sh
+++ csrc/lanes/build-linux32.sh
@@ -1,2 +1,2 @@
 mkdir -p ../../bin/linux32/clib/lanes
-gcc $CFLAGS *.c -shared -o ../../bin/linux32/clib/lanes/core.so -I. -I../lua -pthread -DNDEBUG
+gcc -O2 -s -static-libgcc *.c -shared -o ../../bin/linux32/clib/lanes/core.so -I. -I../lua -pthread -DNDEBUG
