1e97c7e94ccadad17c1cae36b2c5e8fbb03c7f68
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-18 11:47:48 +0400

    mingw64 binaries

--- csrc/lanes/build-mingw32.sh
+++ csrc/lanes/build-mingw32.sh
@@ -1,3 +1,3 @@
-# note: WINVER=0x400 is WinXP (for Vista, use 0x0500 or whatever)
+# NOTE: lanes thinks WinXP is 0x400 not 0x500
 mkdir -p ../../bin/mingw32/clib/lanes
 gcc -O2 -s -static-libgcc *.c -shared -o ../../bin/mingw32/clib/lanes/core.dll -I. -I../lua -L../../bin/mingw32 -llua51 -DNDEBUG -DWINVER=0x0400
