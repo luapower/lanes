3cc1bcd23b46ddce8c4f429d01e15a086e62f501
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-03-23 11:33:56 +0200

    mingw32 build

--- csrc/lanes/build-mingw32.sh
+++ csrc/lanes/build-mingw32.sh
@@ -1,3 +1,3 @@
-# NOTE: lanes thinks WinXP is 0x400 not 0x500
 mkdir -p ../../bin/mingw32/clib/lanes
-gcc -O2 -s -static-libgcc src/*.c -shared -o ../../bin/mingw32/clib/lanes/core.dll -I. -I../lua -L../../bin/mingw32 -llua51 -DNDEBUG -DWINVER=0x0400
+gcc -O2 -s -static-libgcc src/*.c -shared -o ../../bin/mingw32/clib/lanes/core.dll -I. -I../lua \
+	-L../../bin/mingw32 -llua51 -DNDEBUG -DWINVER=0x0501 -D_WIN32_WINNT=0x0501
